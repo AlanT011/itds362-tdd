7d23d3996e9beb0d77ec22ff21f83c8b3b5d0fbb
AlanT011 <krknksungswang@gmail.com>
2026-09-16 23:30:00 +0700

    A5 GREEN: add units to quantity

--- test_kitchen.py
+++ test_kitchen.py
@@ -1,26 +1,22 @@
-# test_kitchen.py
 from kitchen import Quantity
+
+
 def test_multiplication():
-    flour = Quantity(200)
+    flour = Quantity(200, "g")
+    assert flour.times(3) == Quantity(600, "g")
 
-    assert flour.times(3) == Quantity(600)
 
-def test_multiplication_by_two():
-    flour = Quantity(200)
-
-    result = flour.times(2)
-
-    assert result.amount == 400
- 
 def test_multiplication_returns_a_new_quantity():
-    flour = Quantity(200)
+    flour = Quantity(200, "g")
 
-    assert flour.times(3) == Quantity(600)
-    assert flour.times(2) == Quantity(400)
-    
+    assert flour.times(3) == Quantity(600, "g")
+    assert flour.times(2) == Quantity(400, "g")
+
+
 def test_equality():
-    assert Quantity(200) == Quantity(200)
-    assert Quantity(200) != Quantity(300)
- 
+    assert Quantity(200, "g") == Quantity(200, "g")
+    assert Quantity(200, "g") != Quantity(300, "g")
+
+
 def test_grams_are_not_ounces():
     assert Quantity(1, "g") != Quantity(1, "oz")
