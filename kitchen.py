552ff8eb5016bdb0d8db356f438bdd922f63c5e4
AlanT011 <krknksungswang@gmail.com>
2026-09-16 23:09:21 +0700

    A3 GREEN: return new quantity from multiplication

--- kitchen.py
+++ kitchen.py
@@ -1,6 +1,8 @@
  # kitchen.py
 class Quantity:
- def __init__(self, amount):
-  self.amount = amount
- def times(self, multiplier):
-  self.amount = self.amount * multiplier
+    def __init__(self, amount):
+        self.amount = amount
+#  def times(self, multiplier):
+#   self.amount = self.amount * multiplier
+    def times(self, multiplier):
+        return Quantity(self.amount * multiplier)
